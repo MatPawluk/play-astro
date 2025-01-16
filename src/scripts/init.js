500e12ffda4fb515d867f050384d63f57f1b66e0
Mateusz Pawluk <pawluk1988@icloud.com>
2025-01-16 14:52:55 +0100

    f

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,28 +1,57 @@
-function init() {
-  const cookieScript = document.querySelector('#cookieyes');
-  if (cookieScript) {
-    cookieScript.remove();
+document.addEventListener('astro:page-load', () => {
+  function init() {
+    const cookieScript = document.querySelector('#cookieyes');
+    if (cookieScript) {
+      cookieScript.remove();
+    }
+
+    const newScript = document.createElement('script');
+    newScript.id = 'cookieyes';
+    newScript.type = 'text/javascript';
+    newScript.src = 'https://cdn-cookieyes.com/client_data/947994c939c8885fc7bbcabc/script.js';
+
+    document.head.appendChild(newScript);
   }
 
-  const newScript = document.createElement('script');
-  newScript.id = 'cookieyes';
-  newScript.type = 'text/javascript';
-  newScript.src = 'https://cdn-cookieyes.com/client_data/947994c939c8885fc7bbcabc/script.js';
+  document.addEventListener(
+    'astro:before-swap',
+    () => {
+      const oldScript = document.querySelector('#cookieyes');
+      if (oldScript) {
+        oldScript.remove();
+      }
+    },
+    { once: true }
+  );
 
-  document.head.appendChild(newScript);
-}
+  init();
+});
 
-document.addEventListener(
-  'astro:before-swap',
-  () => {
-    const oldScript = document.querySelector('#cookieyes');
-    if (oldScript) {
-      oldScript.remove();
+document.addEventListener('astro:after-swap', () => {
+  function init() {
+    const cookieScript = document.querySelector('#cookieyes');
+    if (cookieScript) {
+      cookieScript.remove();
     }
-  },
-  { once: true }
-);
 
-init();
+    const newScript = document.createElement('script');
+    newScript.id = 'cookieyes';
+    newScript.type = 'text/javascript';
+    newScript.src = 'https://cdn-cookieyes.com/client_data/947994c939c8885fc7bbcabc/script.js';
 
-document.addEventListener('astro:after-swap', init);
+    document.head.appendChild(newScript);
+  }
+
+  document.addEventListener(
+    'astro:before-swap',
+    () => {
+      const oldScript = document.querySelector('#cookieyes');
+      if (oldScript) {
+        oldScript.remove();
+      }
+    },
+    { once: true }
+  );
+
+  init();
+});
