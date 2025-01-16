fcd4f238fb82af3529fc9d9f0c27a496c5a58daa
Mateusz Pawluk <pawluk1988@icloud.com>
2025-01-16 14:55:36 +0100

    f

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,18 +1,18 @@
-document.addEventListener('astro:page-load', () => {
-  function init() {
-    const cookieScript = document.querySelector('#cookieyes');
-    if (cookieScript) {
-      cookieScript.remove();
-    }
-
-    const newScript = document.createElement('script');
-    newScript.id = 'cookieyes';
-    newScript.type = 'text/javascript';
-    newScript.src = 'https://cdn-cookieyes.com/client_data/947994c939c8885fc7bbcabc/script.js';
-
-    document.head.appendChild(newScript);
+function init() {
+  const cookieScript = document.querySelector('#cookieyes');
+  if (cookieScript) {
+    cookieScript.remove();
   }
 
+  const newScript = document.createElement('script');
+  newScript.id = 'cookieyes';
+  newScript.type = 'text/javascript';
+  newScript.src = 'https://cdn-cookieyes.com/client_data/947994c939c8885fc7bbcabc/script.js';
+
+  document.head.appendChild(newScript);
+}
+
+document.addEventListener('astro:page-load', () => {
   document.addEventListener(
     'astro:before-swap',
     () => {
@@ -28,20 +28,6 @@ document.addEventListener('astro:page-load', () => {
 });
 
 document.addEventListener('astro:after-swap', () => {
-  function init() {
-    const cookieScript = document.querySelector('#cookieyes');
-    if (cookieScript) {
-      cookieScript.remove();
-    }
-
-    const newScript = document.createElement('script');
-    newScript.id = 'cookieyes';
-    newScript.type = 'text/javascript';
-    newScript.src = 'https://cdn-cookieyes.com/client_data/947994c939c8885fc7bbcabc/script.js';
-
-    document.head.appendChild(newScript);
-  }
-
   document.addEventListener(
     'astro:before-swap',
     () => {
